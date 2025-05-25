2728cd4c455a69cfe578b64dbfa476db9a3e418a
ldruley <logan.druley@gmail.com>
2025-05-24 22:31:25 -0700

    Added Swagger to NestJS and configured CLI plugin to autogenerate documentation

--- apps/trip-backend/src/main.ts
+++ apps/trip-backend/src/main.ts
@@ -6,9 +6,20 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Trip Planner API')
+    .setDescription('Backend API for Trip Planner Frontend')
+    .setVersion('0.1')
+    .addTag('trip')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
