3225de5645f7559eba3ff34d844230345d770987
ldruley <logan.druley@gmail.com>
2025-05-25 17:06:47 -0700

    began implementing user service

--- apps/trip-backend/src/domain/users/users.service.ts
+++ apps/trip-backend/src/domain/users/users.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException, BadRequestException } from '@nestjs/common';
+import { PrismaService } from '@trip-planner/prisma';
+
+import {
+  CreateProfile,
+  UpdateProfile,
+  Profile,
+  ProfileQuery
+} from '../../../../../libs/shared/types/src/schemas/profile.schema';
 
 @Injectable()
 export class UsersService {}
