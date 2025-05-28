ab38a70f7588341d5a4cd08f86c53f683d8edd26
ldruley <logan.druley@gmail.com>
2025-05-27 20:14:27 -0700

    Trying to setup interceptor with proxy config, not working so hardcoded for the moment

---
+++ apps/trip-frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../../features/auth/services/auth.service';
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  if (req.url.startsWith('http://localhost:3000/api/')) {
+    const session = authService.getCurrentSession();
+    console.log(session);
+    if (session?.access_token) {
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${session.access_token}`
+        }
+      });
+    }
+  }
+
+  return next(req);
+};
