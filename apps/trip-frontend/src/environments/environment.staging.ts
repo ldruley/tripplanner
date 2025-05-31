95e24a3ee864acb31cb215ee502b94ddd85366b0
ldruley <logan.druley@gmail.com>
2025-05-30 20:58:02 -0700

    more deploy config changes

---
+++ apps/trip-frontend/src/environments/environment.staging.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://rvffbudbkqhclfaefjsh.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2ZmZidWRia3FoY2xmYWVmanNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNDA0MjAsImV4cCI6MjA2MzYxNjQyMH0.pRhVxgOV4aj4BvZ0Q5U1vRRY_U9XC62ImrE-vKSOeaY',
+  backendApiUrl: 'http://138.68.5.168/api',
+};
