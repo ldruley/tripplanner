2e3dae9b2a4c8df999ebb69fb9c05176fca37631
ldruley <logan.druley@gmail.com>
2025-06-02 00:31:53 -0700

    refactor: each feature now has it's own routing

---
+++ apps/trip-frontend/src/app/features/auth/auth.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { AuthContainerComponent } from './containers/auth-container/auth-container.component';
+import { authGuard } from '../../core/guards/auth.guard';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'login',
+    component: AuthContainerComponent
+  },
+  {
+    path: 'register',
+    component: AuthContainerComponent
+  },
+  {
+    path: 'change-password',
+    component: AuthContainerComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'forgot',
+    component: AuthContainerComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+    component: AuthContainerComponent
+  },
+];
