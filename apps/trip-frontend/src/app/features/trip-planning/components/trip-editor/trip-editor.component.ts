089d8cfec6f04671309547364cacb24b5e2d6630
ldruley <logan.druley@gmail.com>
2025-06-01 23:22:21 -0700

    chore: scaffolding trip container and editor for trip-planner feature

---
+++ apps/trip-frontend/src/app/features/trip-planning/components/trip-editor/trip-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-trip-editor',
+  imports: [CommonModule],
+  templateUrl: './trip-editor.component.html',
+  styleUrl: './trip-editor.component.css',
+})
+export class TripEditorComponent {}
---
+++ apps/trip-frontend/src/app/features/trip-planning/containers/trip-container/trip-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-trip-container',
+  imports: [CommonModule],
+  templateUrl: './trip-container.component.html',
+  styleUrl: './trip-container.component.css',
+})
+export class TripContainerComponent {}
