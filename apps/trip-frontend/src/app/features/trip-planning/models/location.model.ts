cddaf200bd50c7b545e4dcd6c7c180dcb42e2d97
ldruley <logan.druley@gmail.com>
2025-06-01 23:02:40 -0700

    chore: updating typing for location

---
+++ apps/trip-frontend/src/app/features/trip-planning/models/location.model.ts
@@ -0,0 +1,3 @@
+import { Location as SharedLocation } from '@tripplanner/shared/types';
+
+export type Location = SharedLocation;
--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/types';
 export * from './schemas/profile.schema';
+export * from './schemas/location.schema';
 export * from './dtos/profile.dto';
+export * from './dtos/location.dto';
