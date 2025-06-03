b1a4baac2cf75a29bc7796fccde8fc4f37d33fce
ldruley <logan.druley@gmail.com>
2025-06-03 16:26:34 -0700

    chore: generated itinerary builder component files

---
+++ apps/trip-frontend/src/app/features/trip-planning/components/itinerary-builder/itinerary-builder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-itinerary-builder',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './itinerary-builder.component.html',
+  styleUrl: './itinerary-builder.component.css',
+})
+export class ItineraryBuilderComponent {}
