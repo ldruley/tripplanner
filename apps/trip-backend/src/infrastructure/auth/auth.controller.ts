1dd27ef993c01611055f6c64f4f7fd481ef4aa78
ldruley <logan.druley@gmail.com>
2025-06-08 21:52:16 -0700

    wip: transitioning frontend to camelCase data variables and hooking up backend endpoints to finish supabase transition

--- apps/trip-backend/src/infrastructure/auth/auth.controller.ts
+++ apps/trip-backend/src/infrastructure/auth/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, HttpCode, Post, UseGuards, HttpStatus, Req, Request } from '@nestjs/common';
+import { Controller, HttpCode, Post, UseGuards, HttpStatus, Req, Request, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
-import { SafeUser } from '@trip-planner/types';
+import { CreateUserDto, SafeUser } from '@trip-planner/types';
 
 
 
@@ -15,6 +15,12 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   async login(@Req() req: SafeUser): Promise<{ access_token: string }> {
     return this.authService.login(req);
+  }
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() createUserDto: CreateUserDto): Promise<SafeUser> {
+    return this.authService.register(createUserDto);
   }
 
 
