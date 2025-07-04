98f33017e31ab371c5e226812f806c85b74c5c31
ldruley <logan.druley@gmail.com>
2025-07-03 21:53:46 -0700

    build: missed a few storybook refs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,3 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import nx from '@nx/eslint-plugin';
 
 export default [
@@ -50,5 +47,4 @@ export default [
     // Override or add rules here
     rules: {},
   },
-  ...storybook.configs["flat/recommended"]
 ];
